3ed3587089bf0ac73b379dda5d94a783a084747f
Andrew Miracle <me@andrewmiracle.com>
2019-10-14 14:16:51 +0000

    implement create and Read functionalit

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
 
 // import Tasks Model from server
@@ -17,21 +18,44 @@ class App extends Component {
         ];
     }
 
-    renderTasks() {
+    renderTasks = () => {
         return this.props.tasks.map((task) => ( 
             <Task key={task._id} task={task} />
         ));
     }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim()        
+        
+        Tasks.insert({
+            text,
+            createdAt: new Date()
+        });
+        
+        // clear the form
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    };
 
     render() {
         return (
           <div className="container">
             <header>
               <h1>Todo List</h1>
-            </header>    
+            {/* Add a form to the app */}
+            <form className="new-task" onSubmit={this.handleSubmit}>
+                <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+                {/* <button type="submit">submit</button> */}
+            </form>
+            
+            </header>
+                
+
             <ul>
               {this.renderTasks()}
             </ul>
+
+            
           </div>
         );
       }
