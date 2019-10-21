a2d5690cdf719102f9bfab45ccbed1a00dbf9f3f
Andrew Miracle <me@andrewmiracle.com>
2019-10-21 06:19:18 +0000

    implement iron router

--- meteor/eit-manager/client/main.js
+++ meteor/eit-manager/client/main.js
@@ -1,2 +1,3 @@
-import '../imports/startup/accounts-config.js';
-import '../imports/ui/body.js';
+import '../imports/startup/accounts-config';
+import '../imports/ui/body';
+import '../imports/routes'
---
+++ meteor/eit-manager/imports/routes.js
@@ -0,0 +1,8 @@
+Router.route('/', function () {
+    this.render('Home');
+  });
+  
+  Router.route('/eit', function () {
+    this.render('eitList');
+  });
+  
